e6a8a03551772f73abe38d0fbc937620584585df
Benishimizu <mythica4624@gmail.com>
2024-01-16 00:21:44 +0900

    必要画面設計

---
+++ App/app/next/src/app/senior/senior_login.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link';
+
+// シンプルなヘッダーコンポーネント
+const Header = () => {
+  return (
+    <header className="text-center py-4 bg-gray-200">
+      {/* ここにヘッダーのコンテンツを追加 */}
+      みまもり
+
+      <Link href="/register" passHref>
+      新規登録
+      </Link>
+
+      <Link href="/senior-login" passHref>
+      シニア用ログイン
+      </Link>
+
+      <Link href="/login" passHref>
+      ご家族用ログイン
+      </Link>
+
+      
+    </header>
+  );
+};
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      {/* ヘッダーの呼び出し */}
+      <Header />
+
+      {/* メインコンテンツ */}
+      <main className="flex-grow flex flex-col items-center p-24">
+        <p>シニアの方のログインページです</p>
+        
+      </main>
+
+      {/* フッターの呼び出し */}
+    </div>
+  );
+}
