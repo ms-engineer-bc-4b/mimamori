3a1dcb9c7b49dbd1b9388051af51165dbb3fabf2
Benishimizu <mythica4624@gmail.com>
2024-01-19 00:41:00 +0900

    モデルの作成
    postmanをするためのモデルの作成

---
+++ App/FLASK_SQLALCHEMY/api_server/models/__pycache__/senior_user.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f"<User(id={self.id}, email={self.email})>"
