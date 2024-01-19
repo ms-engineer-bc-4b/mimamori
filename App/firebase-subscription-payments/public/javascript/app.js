3269fb019f16db6a15f061e62cfe2b1a380ad1d8
MaiOide <mai.09055752288@gmail.com>
2024-01-20 07:58:43 +0900

    add firebase-subscription-payments!

---
+++ App/firebase-subscription-payments/public/javascript/app.js
@@ -0,0 +1,225 @@
+const prices = {};
+
+// Replace with your Firebase project config.
+const firebaseConfig = {
+  apiKey: "AIzaSyCKE6MIRYKsIiHiXbLzmg2-kC_Asdk2OOo",
+  authDomain: "mimamori-40e2d.firebaseapp.com",
+  projectId: "mimamori-40e2d",
+  storageBucket: "mimamori-40e2d.appspot.com",
+  messagingSenderId: "268924022546",
+  appId: "1:268924022546:web:b05f3ab2c7192010534dcb",
+  measurementId: "G-DE99T039CN"
+};
+
+// Replace with your cloud functions location
+const functionLocation = 'asia-northeast1';
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+/**
+ * Firebase Authentication configuration
+ */
+const firebaseUI = new firebaseui.auth.AuthUI(firebase.auth());
+const firebaseUiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      return true;
+    },
+    uiShown: () => {
+      document.querySelector('#loader').style.display = 'none';
+    },
+  },
+  signInFlow: 'popup',
+  signInSuccessUrl: '/',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
+  credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+  // Your terms of service url.
+  tosUrl: 'https://example.com/terms',
+  // Your privacy policy url.
+  privacyPolicyUrl: 'https://example.com/privacy',
+};
+firebase.auth().onAuthStateChanged((firebaseUser) => {
+  if (firebaseUser) {
+    document.querySelector('#loader').style.display = 'none';
+    document.querySelector('main').style.display = 'block';
+    currentUser = firebaseUser.uid;
+    startDataListeners();
+  } else {
+    document.querySelector('main').style.display = 'none';
+    firebaseUI.start('#firebaseui-auth-container', firebaseUiConfig);
+  }
+});
+
+/**
+ * Data listeners
+ */
+function startDataListeners() {
+  // Get all our products and render them to the page
+  const products = document.querySelector('.products');
+  const template = document.querySelector('#product');
+  db.collection('products')
+    .where('active', '==', true)
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach(async function (doc) {
+        const priceSnap = await doc.ref
+          .collection('prices')
+          .where('active', '==', true)
+          .orderBy('unit_amount')
+          .get();
+        if (!'content' in document.createElement('template')) {
+          console.error('Your browser doesn’t support HTML template elements.');
+          return;
+        }
+
+        const product = doc.data();
+        const container = template.content.cloneNode(true);
+
+        container.querySelector('h2').innerText = product.name.toUpperCase();
+        container.querySelector('.description').innerText =
+          product.description?.toUpperCase() || '';
+        // Prices dropdown
+        priceSnap.docs.forEach((doc) => {
+          const priceId = doc.id;
+          const priceData = doc.data();
+          prices[priceId] = priceData;
+          const content = document.createTextNode(
+            `${new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: priceData.currency,
+            }).format((priceData.unit_amount / 100).toFixed(2))} per ${
+              priceData.interval ?? 'once'
+            }`
+          );
+          const option = document.createElement('option');
+          option.value = priceId;
+          option.appendChild(content);
+          container.querySelector('#price').appendChild(option);
+        });
+
+        if (product.images.length) {
+          const img = container.querySelector('img');
+          img.src = product.images[0];
+          img.alt = product.name;
+        }
+
+        const form = container.querySelector('form');
+        form.addEventListener('submit', subscribe);
+
+        products.appendChild(container);
+      });
+    });
+  // Get all subscriptions for the customer
+  db.collection('customers')
+    .doc(currentUser)
+    .collection('subscriptions')
+    .where('status', 'in', ['trialing', 'active'])
+    .onSnapshot(async (snapshot) => {
+      if (snapshot.empty) {
+        // Show products
+        document.querySelector('#subscribe').style.display = 'block';
+        return;
+      }
+      document.querySelector('#subscribe').style.display = 'none';
+      document.querySelector('#my-subscription').style.display = 'block';
+      // In this implementation we only expect one Subscription to exist
+      const subscription = snapshot.docs[0].data();
+      const priceData = (await subscription.price.get()).data();
+      document.querySelector(
+        '#my-subscription p'
+      ).textContent = `You are paying ${new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: priceData.currency,
+      }).format((priceData.unit_amount / 100).toFixed(2))} per ${
+        priceData.interval
+      }, giving you the role: ${await getCustomClaimRole()}. 🥳`;
+    });
+}
+
+/**
+ * Event listeners
+ */
+
+// Signout button
+document
+  .getElementById('signout')
+  .addEventListener('click', () => firebase.auth().signOut());
+
+// Checkout handler
+async function subscribe(event) {
+  event.preventDefault();
+  document.querySelectorAll('button').forEach((b) => (b.disabled = true));
+  const formData = new FormData(event.target);
+  const selectedPrice = {
+    price: formData.get('price'),
+  };
+  // For prices with metered billing we need to omit the quantity parameter.
+  // For all other prices we set quantity to 1.
+  if (prices[selectedPrice.price]?.recurring?.usage_type !== 'metered')
+    selectedPrice.quantity = 1;
+  const checkoutSession = {
+    automatic_tax: true,
+    tax_id_collection: true,
+    collect_shipping_address: true,
+    allow_promotion_codes: true,
+    line_items: [selectedPrice],
+    success_url: window.location.origin,
+    cancel_url: window.location.origin,
+    metadata: {
+      key: 'value',
+    },
+  };
+  // For one time payments set mode to payment.
+  if (prices[selectedPrice.price]?.type === 'one_time') {
+    checkoutSession.mode = 'payment';
+    checkoutSession.payment_method_types = ['card', 'sepa_debit', 'sofort'];
+  }
+
+  const docRef = await db
+    .collection('customers')
+    .doc(currentUser)
+    .collection('checkout_sessions')
+    .add(checkoutSession);
+  // Wait for the CheckoutSession to get attached by the extension
+  docRef.onSnapshot((snap) => {
+    const { error, url } = snap.data();
+    if (error) {
+      // Show an error to your customer and then inspect your function logs.
+      alert(`An error occured: ${error.message}`);
+      document.querySelectorAll('button').forEach((b) => (b.disabled = false));
+    }
+    if (url) {
+      window.location.assign(url);
+    }
+  });
+}
+
+// Billing portal handler
+document
+  .querySelector('#billing-portal-button')
+  .addEventListener('click', async (event) => {
+    document.querySelectorAll('button').forEach((b) => (b.disabled = true));
+
+    // Call billing portal function
+    const functionRef = firebase
+      .app()
+      .functions(functionLocation)
+      .httpsCallable('ext-firestore-stripe-subscriptions-createPortalLink');
+    const { data } = await functionRef({ returnUrl: window.location.origin });
+    window.location.assign(data.url);
+  });
+
+// Get custom claim role helper
+async function getCustomClaimRole() {
+  await firebase.auth().currentUser.getIdToken(true);
+  const decodedToken = await firebase.auth().currentUser.getIdTokenResult();
+  return decodedToken.claims.stripeRole;
+}
