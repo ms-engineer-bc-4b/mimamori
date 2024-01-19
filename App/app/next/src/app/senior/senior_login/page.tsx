c0651aa3579fd60969b2d9196c625e635ba09caa
Benishimizu <mythica4624@gmail.com>
2024-01-19 22:50:11 +0900

    ログインページある程度完成
    CSS整理したいコード
    .my-input {
      width: 381.82px;
      height: 52.48px;
      left: 117.18px;
      top: 0;
      position: absolute;
      background-color: white;
      border: 1px solid black;
    }
    
    <input className="input is-large my-input">

--- App/app/next/src/app/senior/senior_login/page.tsx
+++ App/app/next/src/app/senior/senior_login/page.tsx
@@ -5,28 +5,31 @@ const Header = () => {
             return (
             <header className="text-center py-4 bg-gray-200">
             {/* ここにヘッダーのコンテンツを追加 */}
-            <div class="left-[96px] top-[8px] absolute text-green-950 text-[64px] font-bold font-['Zen Maru Gothic']">
-            みまもり
+            <div className="left-[96px] top-[8px] absolute text-green-950 text-[64px] font-bold font-['Zen Maru Gothic']">
+             みまもり
             </div>
-            <div class="left-[768px] top-[69px] absolute text-green-950 text-lg font-normal font-['Inter']">
-            <Link href="/register">
-            新規登録
-            </Link>
+            <div className="left-[768px] top-[69px] absolute text-green-950 text-lg font-normal font-['Inter']">
+              <Link href="/register">
+                新規登録
+              </Link>
             </div>
-            <div class="left-[938px] top-[69px] absolute text-green-950 text-lg font-normal font-['Inter']">
-            <Link href="/senior-login">
-            シニア用ログイン
-            </Link>
+            <div className="left-[900px] top-[69px] absolute text-green-950 text-lg font-normal font-['Inter']">
+              <Link href="/senior/senior_login">
+                シニア用ログイン
+              </Link>
             </div>
-            <div class="left-[1100px] top-[69px] absolute text-green-950 text-lg font-normal font-['Inter']">
-            <Link href="/login">
-            ご家族用ログイン
-            </Link>
+            <div className="left-[1100px] top-[69px] absolute text-green-950 text-lg font-normal font-['Inter']">
+              <Link href="/login">
+                ご家族用ログイン
+              </Link>
             </div>
+
+            <div className="w-[1188px] h-[703px] left-[6px] top-0 absolute">
+            </div>
+
             </header>
             );
   };
-
 
 export default function SeniorLogin() {
   return (
@@ -35,89 +38,59 @@ export default function SeniorLogin() {
       <Header />
 
       {/* メインコンテンツ */}
-      <main className="flex-grow flex flex-col items-center p-24">
+      <main className="flex-grow flex flex-col items-center">
+      <div className="w-[220px] h-[23px] left-[500px] top-[233px] absolute text-black text-xl font-normal font-['Inter']">
         <p>シニアの方のログインページです</p>
-        {/* // シニアの方のページです */}
-          {/* text-black
-            text-xl
-            font-normal
-            font-['Inter']
-            --- */}
-
-
-        ID
-
-          {/* // ID
-          text-green-950
-          text-xl
-          font-normal
-          font-['Inter'] */}
-
-        パスワード
-
-          {/* // パスワード
-          text-green-950
-          text-xl
-          font-normal
-          font-['Inter'] */}
-
-        ログイン
-          {/* // ログイン
-          text-black
-          text-[25px]
-          font-normal
-          font-['Inter']
-          --- */}
-
-
-
-      {/* CSS */}
-      <div class="w-[1200px] h-[2000px]">
-        
-          <div class="w-[1200px] h-[2000px] p-2 left-0 top-0 absolute bg-gradient-to-b from-yellow-200 to-yellow-300 border border-black"></div>
-          <div class="w-[1188px] h-[703px] left-[6px] top-0 absolute">
-            
-              <div class="w-[220px] h-[23px] left-[497px] top-[233px] absolute text-black text-xl font-normal font-['Inter']">シニアの方のページです</div>
-              <div class="w-[499px] h-[52.48px] left-[367px] top-[277px] absolute">
-                  <div class="w-[499px] h-[52.48px] left-0 top-0 absolute">
-                      <div class="w-[117.18px] h-[51.72px] left-0 top-0 absolute bg-zinc-300 border border-black"></div>
-
-                      <input class="input is-large" type="text" name="name" placeholder="Name" autofocus=""></input>
-
-                      {/* 入力フォーム */}
-                      {/* <div input 
-                          type="text" 
-                          onInput={handleInputText} //関数の指定必要
-                          value={String(text)}
-                          className={styles.item}
-                        /> */}
-                        {/* CSSで作成分 */}
-                      <div class="w-[381.82px] h-[52.48px] left-[117.18px] top-0 absolute bg-white border border-black"></div>
-                  </div>
-                  <div class="w-[69.35px] h-[19.78px] left-[48px] top-[16.61px] absolute text-green-950 text-xl font-normal font-['Inter']">ID</div>
-              </div>
-              <div class="w-[499px] h-[51px] left-[367px] top-[348px] absolute">
-                  <div class="w-[499px] h-[51px] left-0 top-0 absolute">
-                      <div class="w-[117.18px] h-[50.26px] left-0 top-0 absolute bg-zinc-300 border border-black"></div>
-                      
-                      {/* 入力フォーム */}
-                      <div class="w-[381.82px] h-[51px] left-[117.18px] top-0 absolute bg-white border border-black"></div>
-                  </div>
-                  <div class="w-28 h-[18px] left-[10px] top-[16px] absolute text-green-950 text-xl font-normal font-['Inter']">パスワード</div>
-              </div>
-              <div class="w-[261px] h-[65px] left-[516px] top-[424px] absolute">
-                  <div class="w-[261px] h-[65px] left-0 top-0 absolute bg-zinc-300 rounded-[10px] shadow"></div>
-                  <div class="left-[79px] top-[18px] absolute text-black text-[25px] font-normal font-['Inter']">ログイン</div>
-              </div>
-              {/* <img class="w-[45px] h-[41px] left-[463px] top-[436px] absolute" src="https://via.placeholder.com/45x41" /> */}
-
-
-
-          </div>
       </div>
 
 
+      {/* 背景 */}
+      {/* 全体的な構成 */}
+        <div className="w-[1200px] h-[2000px]">
+          {/* 黄色背景*/}
+          {/* <div className="w-[1200px] h-[2000px] p-2 left-0 top-0 absolute bg-gradient-to-b from-yellow-200 to-yellow-300 border border-black"></div> */}
+            <div className="w-[1188px] h-[703px] left-[6px] top-0 absolute">
+              {/* ID */}
+                <div className="w-[499px] h-[52.48px] left-[367px] top-[277px] absolute">
+                    <div className="w-[499px] h-[52.48px] left-0 top-0 absolute">
+                        <div className="w-[117.18px] h-[51.72px] left-0 top-0 absolute bg-zinc-300 border border-black">
+                        </div>
+                          </div>
+                          {/*ID  */}
+                            <div className="w-[69.35px] h-[19.78px] left-[48px] top-[16.61px] 
+                            absolute text-green-950 text-xl font-normal font-['Inter']">
+                              ID
+                            </div>
+                            {/* ID入力フォーム */}
+                            <input className="input is-large w-[381.82px] h-[52.48px] left-[117.18px] top-0 absolute bg-white border border-black">
+                            </input>
+                </div>
+              {/* パスワード */}
+                <div className="w-[499px] h-[51px] left-[367px] top-[348px] absolute">
+                    <div className="w-[499px] h-[51px] left-0 top-0 absolute">
+                        <div className="w-[117.18px] h-[50.26px] left-0 top-0 absolute bg-zinc-300 border border-black">
+                        </div>
+                        {/* 入力フォーム */}
+                    </div>
+                    <div className="w-28 h-[18px] left-[10px] top-[16px] absolute text-green-950 text-xl font-normal font-['Inter']">
+                      パスワード
+                    </div>
 
+                    <input className="input is-large w-[381.82px] h-[52.48px] left-[117.18px] top-0 absolute bg-white border border-black">
+                    </input>
+                </div>
+                <div className="w-[261px] h-[65px] left-[516px] top-[424px] absolute">
+                    {/* <img className="w-[45px] h-[41px] left-[463px] top-[436px] absolute" src="https://via.placeholder.com/45x41" /> */}
+                    <div className="w-[261px] h-[65px] left-0 top-0 absolute bg-zinc-300 rounded-[10px] shadow">
+                    </div>
+                    <div className="left-[79px] top-[18px] absolute text-black text-[25px] font-normal font-['Inter']">
+                    <Link href="/senior/senior_top_page" passHref>
+                      ログイン
+                    </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
       </main>
 
       {/* フッターの呼び出し */}
