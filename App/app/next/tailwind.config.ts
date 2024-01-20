02cc846e64ed9058e85657fd5a6f2224dcfdd691
Benishimizu <mythica4624@gmail.com>
2024-01-20 13:59:18 +0900

    背景一括実装
    背景一括実装のため
    
            // このファイルがどういうものかどういうことができるかを調べる

--- App/app/next/tailwind.config.ts
+++ App/app/next/tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      backgroundColor:{
+        // 'bg-gradient-to-b: (from-yellow-200 to-yellow-300'),
+        // このファイルがどういうものかどういうことができるかを調べる
+      },
     },
   },
   plugins: [],
