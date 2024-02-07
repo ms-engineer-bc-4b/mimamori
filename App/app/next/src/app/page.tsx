ba909878af0b02ae64f1101cdfd46c64d1ad0c77
MaiOide <mai.09055752288@gmail.com>
2024-02-08 00:21:59 +0900

    発表用に修正したものをアップ

--- App/app/next/src/app/page.tsx
+++ App/app/next/src/app/page.tsx
@@ -6,10 +6,14 @@ import { FaUserCircle, FaUserPlus } from "react-icons/fa";
 import './globals.css';
 import Header from '@/components/Header';
 import { createContext } from 'vm';
+import Image from 'next/image'
+
 //import Calendar from '@/components/Calendar';
 //import VoiceToText from '@/components/VoiceToText';
 
 // シンプルなヘッダーコンポーネント
+
+import '@fontsource/zen-maru-gothic';
 
 // Home コンポーネント
 export default function Home() {
@@ -22,38 +26,51 @@ export default function Home() {
     <div className="form-container   ">
 
       {/* 背景のdiv */}
-    
+
       <Header />
-          {/*  <Calendar />*/}
-    
+      {/*  <Calendar />*/}
+
 
       {/* メインコンテンツ */}
       <main className={`toppage`}>
 
-        
-        <div className="text-green-950 text-3xl font-normal font-['Zen Maru Gothic']">遠くにいても安心。<br /></div>
-        <div className="text-green-950 text-xl font-normal font-['Zen Maru Gothic']">みまもりは高齢者と家族をつなぐ、安心のコミュニケーションアプリです<br /></div>
-        <div className={`flex-grow flex flex-col  p-24 relative z-10 items-center`}>
-        <div className="text-black text-[25px] font-normal font-['Inter']">新規登録はこちらから</div>
-  
-        {/* 新規登録画面へのリンク */}
      
-        <button className="flex items-center bg-gray-500 border text-white p-3 rounded-full">
-    <Link href="/register" passHref>
-      <div className="flex items-center">
-        <FaUserPlus className="text-3xl" />
-        
-        <div className="text-lg font-normal font-['Inter'] ml-2">
-          新規登録
-        </div>
-      </div>
-    </Link>
-  </button>
+        <div className="text-green-950 text-3xl font-normal font-['Zen Maru Gothic']" style={{ fontFamily: 'Zen Maru Gothic', fontSize: '30px', fontWeight: 'bold' }}>遠くにいても安心。<br /></div>
+        <div className="text-green-950 text-xl font-normal font-['Zen Maru Gothic'] " style={{ fontFamily: 'Zen Maru Gothic', fontSize: '20px', fontWeight: 'bold' }}>みまもりは高齢者と家族をつなぐ、安心のコミュニケーションアプリです<br /></div>
+        <div className={`flex-grow flex flex-col  p-24 relative z-10 items-center`}>
+        <Image
+            src="/mimamori-top.png"
+            alt="トップ画面"
+            width={500}
+            height={500}
+            priority
+          />
+              <div className="text-black text-[25px] font-normal font-['Inter']"></div>
+              <div style={{ marginTop: '20px' }}>
+          <div className="text-black text-[25px] font-normal font-['Inter']" style={{ fontFamily: 'Zen Maru Gothic', fontSize: '20px', fontWeight: 'bold' }}>新規登録はこちらから</div>
+
+          {/* 新規登録画面へのリンク */}
+          </div>
+         
+          <div style={{ marginTop: '20px' }}>
+             <button className="flex items-center bg-gray-500 border text-white p-3 rounded-full">
+            <Link href="/register" passHref>
+              <div className="flex items-center">
+                <FaUserPlus className="text-3xl" />
+              
+                <div className="text-lg font-normal font-['Inter'] ml-2">
+                  新規登録
+                </div>
+              </div>
+            </Link>
+          </button>
+          </div>
         </div>
       </main>
-  
+
       {/* フッターの呼び出し */}
-   
+
     </div>
-  
-  );}
+
+  );
+}
